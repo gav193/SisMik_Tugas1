9e12cb8e73c026ad790dd70d70b1401b6e94e78d
gav193 <116613315+gav193@users.noreply.github.com>
2024-10-15 10:10:25 +0700

    Create Blink.ino

---
+++ Blink.ino
@@ -0,0 +1,21 @@
+#define LED 2 // set LED pin to D2
+
+int onTime = 800;  // Time LED stays ON
+int offTime = 1600; // Time LED stays OFF
+
+void setup() {
+  // set LED as output
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(LED, HIGH);
+  delay(onTime); // LED set to HIGH for 800ms
+  
+  digitalWrite(LED, LOW);
+  delay(offTime); // LED set to LOW for 1600ms
+
+  // period = 2400ms
+  // duty cycle = 800 / 2400 = 33.3%
+}
